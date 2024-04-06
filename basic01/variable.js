7248a188f9f78aba01decacf6c0f37501e14730e
Ayushman Rout <161948217+ayushm001@users.noreply.github.com>
2024-04-06 22:06:07 +0530

    understanding variable

---
+++ basic01/variable.js
@@ -0,0 +1,16 @@
+const accountId = 101
+let accountEmail = "ayushman@2003"
+var  accountpassword = "12345"
+
+
+
+// console.log(accountId);
+
+
+
+accountEmail= "ayush@google.com"
+accountpassword=  "555555"
+accountId= "200"
+
+
+console.table ([ accountId, accountEmail, accountpassword]);
