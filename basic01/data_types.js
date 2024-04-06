3e4449f4c1a1d1ac5ee6913e050c0d1741e0b77e
Ayushman Rout <161948217+ayushm001@users.noreply.github.com>
2024-04-06 23:09:18 +0530

    understanding  datatypes
     Table specification  datatypes role , Null value , Undefined function and Object

---
+++ basic01/data_types.js
@@ -0,0 +1,18 @@
+console.log (3+3)
+
+
+let StudentName = "Ayushman"
+let Studentage= 20
+let IsLoggedIn = true
+let payment_status = null 
+let Stud_Due;         
+
+//(NULL - TYPE OF STANDALONE VALUE OTERWISE  REPRESENTATION OF A EMPITY VALUE , ALSO AN OBJECT ) 
+// ( UNDEFINED STANDS FOR VALUES WHICH ARE NOT ASSIGNED)
+
+console.table([StudentName,payment_status]);
+
+
+console.log(typeof IsLoggedIn)
+console.log(typeof Stud_Due)
+console.log(typeof null)
