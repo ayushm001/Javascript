4ae65f1e3069edcab08c7d9eda3033ad80576775
Ayushman Rout <161948217+ayushm001@users.noreply.github.com>
2024-04-08 10:12:03 +0530

    Update ConvrtionOperation.js

--- basic01/ConvrtionOperation.js
+++ basic01/ConvrtionOperation.js
@@ -1,7 +1,25 @@
-let score= 20
+ let score= "2xyz0"
+ var AccountName= "AYUSHMAN "
 
-var accntName = "ROUT"
 
 
-console.log(typeof score);
-connsol.log(typeof ( accntName ));
+
+ console.log(typeof  score);
+ console.log(typeof  (  AccountName));
+
+
+ let valueinNumber= Number(score);
+
+
+ console.log(AccountName);
+
+ console.log( valueinNumber);        //(Here the result will be NaN  & if the score i/p is NULL then te result will be 0 , if the score i/p is BOOLEAN VALUE then te result will be 1 (true) or 0 (false) )
+
+
+
+let Customer = "ayushman rout"
+let UNcle = Boolean(Customer )
+
+console.log (typeof UNcle)        //if  the i/p is "" => False ,1=> TRUE, 0=> FALSE , "anybcs" => TRUE
+console.log ( UNcle)
+
