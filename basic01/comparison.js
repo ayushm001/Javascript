6d785b8d106580a5e9c96d7e8ddf7bf6bcf10ea2
Ayushman Rout <161948217+ayushm001@users.noreply.github.com>
2024-04-09 00:39:59 +0530

    understandings  null ,undefined, numb's different operations in different condition's.

---
+++ basic01/comparison.js
@@ -0,0 +1,34 @@
+ console.log ("3"> 1)
+ console.log(3<5)
+
+   //-------------------------------------------------//
+
+
+ console.log(2 > 1);
+ console.log(2 >= 1);
+ console.log(2 < 1);
+ console.log(2 == 1);
+ console.log(2 != 1);
+ console.log("2" > 1);
+ console.log("02" > 1);
+
+   //-------------------------------------------------//
+
+
+  console.log(null > 0);            // FALSE cause null is a empity value 
+  console.log(null == 0);           // here the value is not equal to the both side 
+  console.log(null >= 0);           // here  it treats null as 0, so 0=0 condition satisfied , so the o/p is TRUE
+  console.log(null <= 0);          // SAME HERE
+
+    //-------------------------------------------------//
+
+   console.log(undefined == 0);
+   console.log(undefined > 0);
+   console.log(undefined < 0);
+   console.log(undefined >= 0);
+   console.log(undefined <= 0);
+
+  // ===         {HERE IT CHECKS THE VALUES STRICTLY}
+
+console.log("2" === 2);
+
