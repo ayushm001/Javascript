4e4ccc8f4a2be35dc3ff0ea711db6a087a7e2920
Ayushman Rout <161948217+ayushm001@users.noreply.github.com>
2024-04-08 16:35:09 +0530

    understanding string , numb operations . Postfix & Prefix operation

---
+++ basic01/operation.js
@@ -0,0 +1,37 @@
+ let ab= "10"
+     ac = 10
+
+
+     let xy = ab+ac;
+
+     console.log(xy);
+     console.log(typeof xy);
+
+
+
+ console.log("1"+"2"+3);   //here the Result is 123,  cause "1" and "2" are strings, and the third operand (3) is a number.When at least one operand is a string, the + operator performs concatenation instead of addition. So, "1" + "2" results in the string "12", and then "12" + 3 results in "123" due to the string concatenation. Therefore, the result is "123".
+
+ console.log(1+2+3);       //here the Result is 6 ,  these all are Numbers so it performs addititon
+
+ console.log(1+2+"3");     //here the Result is 33, here the string value ("3") so the firs operation will be done after that at will beacome string (1+2), then the result will be 33
+
+ console.log(1+"2"+3);     //here the Result is 123,  here the numb is add withh string then it will be 12 and it add with numb again . then the result will be 123
+
+ console.log("1"+2+3);     //here the Result is 123,  here the string is in the starting part so the rest operants (2&3) will also be consider as string .
+
+
+console.log(+ true)
+
+console.log(+ "")    // resut is 0 
+
+let x1 , x2 , x3 
+
+x1 = x2 = x3 = 2+3
+
+console.log(x2);
+
+
+let xq1 = 20
+++xq1;                 //21
+console.log(xq1)    
+
