dcaae60006ce6cc94ebee0c9dc17cf08f3af7040
Ayushman Rout <161948217+ayushm001@users.noreply.github.com>
2024-04-10 23:43:06 +0530

    Add files via upload

---
+++ basic01/datatypes_summery.js
@@ -0,0 +1,12 @@
+function increment(x) {
+    x += 1;
+    console.log("Inside function:", x);
+}
+
+// Define a variable
+let num = 5;
+
+// Call the function with the variable
+increment(num);
+
+console.log(typeof num)
