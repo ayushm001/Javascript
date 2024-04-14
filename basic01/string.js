f461c95677785716c825cdfa4759d3f9e0375653
Ayushman Rout <161948217+ayushm001@users.noreply.github.com>
2024-04-14 23:41:07 +0530

    Update string.js

--- basic01/string.js
+++ basic01/string.js
@@ -2,15 +2,34 @@ const Name =  "Ayushman "
 const repoCount = 30
 
 
-// console.log (Name + repoCount + "Value"); out deted sytax not to use
+console.log (Name + repoCount + "Value");
+//  out deted sytax not to use
 
 
 console.log (`Hello my name is ${Name} and my repo count is ${repoCount}`);
-
-//  (``) is used for string inter polletion
+// This syntx consider as a good synatx so called morden syntax . 
+//  (``) is used for String interpolation 
+//  String interpolation - is used for combineing texts & variables. where {} are placeholders for the variables.
 //  ${} - Used for variable declartion 
 
-const GameName = new String ('AYUSHHMAN')
-
+const GameName = new String ('Ayushman')
+//  here the variable GameName plays the role of an object insted of a string 
+console.log (typeof GameName)
 console.log(GameName [1])  
 //  HERE THE RESULT WILL BE 'Y'
+console.log(GameName.__proto__);
+// 
+console.log(GameName.length ) 
+//  to declare the variables length
+
+console.log(GameName.toUpperCase ())  
+// to convert the variables in to uppercase
+
+console.log(GameName.charAt(3) )  
+//  To find the variables data in the given position
+
+console.log(GameName.indexOf ('n'))  
+//  To find the variables positon in the given data
+
+console.log(GameName [1])  
+
