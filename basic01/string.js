a32c112fbe12c7b8432fc387aaf2eb9946725c46
Ayushman Rout <161948217+ayushm001@users.noreply.github.com>
2024-04-14 23:05:58 +0530

    Update string.js

--- basic01/string.js
+++ basic01/string.js
@@ -1,7 +1,16 @@
 const Name =  "Ayushman "
 const repoCount = 30
 
-console.log (Name + repoCount + "Value");
+
+// console.log (Name + repoCount + "Value"); out deted sytax not to use
 
 
-console.log ()
+console.log (`Hello my name is ${Name} and my repo count is ${repoCount}`);
+
+//  (``) is used for string inter polletion
+//  ${} - Used for variable declartion 
+
+const GameName = new String ('AYUSHHMAN')
+
+console.log(GameName [1])  
+//  HERE THE RESULT WILL BE 'Y'
