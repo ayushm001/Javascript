05bdb916f65cb2f2f2ef2cfba23fc99347306305
Ayushman Rout <161948217+ayushm001@users.noreply.github.com>
2024-04-18 21:40:22 +0530

    Update array_01.js

--- basic02/array_01.js
+++ basic02/array_01.js
@@ -5,3 +5,14 @@
 
 const myArray =  [0, 3, 5, 2, ]
 console.log(myArray);
+console.log(myArray[2]);   // here it prints the result in index value order
+
+const Peoples =  ["Akhram", "Vivek", "Banshika", 3, 8]
+console.log(Peoples[1]);
+
+const NewArray1 = [ 1, 2, 3, 4, 5]
+console.log(NewArray1[0]);
+
+
+// Array Methods :-
+
