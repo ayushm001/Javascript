c61e41772611b0021021578051798a2c5a27ff79
Ayushman Rout <161948217+ayushm001@users.noreply.github.com>
2024-04-20 00:51:30 +0530

    understanding Array and its methods, slice & splice

--- basic02/array_01.js
+++ basic02/array_01.js
@@ -14,7 +14,7 @@ const NewArray1 = [ 1, 2, 3, 4, 5]
 console.log(NewArray1[0]);
 
 
-// Array Methods :-
+// // Array Methods :-
 myArray.push(8);          // 'push'method use to put the value in the Array
 myArray.push(3);         
 myArray.pop();          // 'pop' use to pull the last index value in the Array
@@ -23,35 +23,31 @@ console.log(myArray.includes(2));   //.includes define whether the value is exia
 console.log(myArray.indexOf (2));   // Define the index value 
 
 
-const NewArray= myArray.join()    //.join 
-
-
-/ console.log(myArr.includes(9));
-// console.log(myArr.indexOf(3));
-
-// const newArr = myArr.join()
-
-// console.log(myArr);
-// console.log( newArr);
-
-
-// slice, splice
-
-console.log("A ", myArr);
-
-const myn1 = myArr.slice(1, 3)
-
-console.log(myn1);
-console.log("B ", myArr);
-
-
-const myn2 = myArr.splice(1, 3)
-console.log("C ", myArr);
-console.log(myn2);
+const NewArray= myArray.join()    //.join Adds all the elements of an array into a string, separated by the specified separator string.
 
 console.log( NewArray);     
-console.log( typeof NewArray);
-
+console.log( typeof NewArray);     // convert into string 
 console.log( myArray);
 
- 
+// -----------Slice , Splice ------------
+
+// ---------slice----------
+
+const arryX = [0, 1, 2, 3, 4, 5]
+console.log("A ", arryX);
+const myArray_Y= arryX.slice (1, 3);
+console.log(myArray_Y);
+console.log(arryX);
+
+//-----> Slice: Gives you a portion without changing the original.
+//  thats why the result will be [ 1, 2 ]. but its main obj "arryX" has its orignal value 
+
+
+//  ----------splice----------
+console.log("B ", arryX);
+const myArray_Z=  arryX.splice (1, 3);
+console.log(myArray_Z);
+console.log(arryX);
+
+//-----> Splice: Modifies the original directly by adding, removing, or rearranging elements.
+//  But here the result will be print as [ 1, 2, 3 ]. and its orignal obj "arryX" has only left value i.e:- [ 0, 4, 5 ]
