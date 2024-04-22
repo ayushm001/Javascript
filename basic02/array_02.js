0408f0388415d0079755c1fe01de582aa8250f65
Ayushman Rout <161948217+ayushm001@users.noreply.github.com>
2024-04-23 04:42:47 +0530

    Update array_02.js

--- basic02/array_02.js
+++ basic02/array_02.js
@@ -3,12 +3,15 @@ const dc_heros = ["superman", "flash", "batman"]
 
 marvel_heros.push(dc_heros);
 console.log(marvel_heros);
-console.log(marvel_heros.indexOf("Ironman"));
+console.log(marvel_heros.indexOf("Ironman")); 
+
 console.log(marvel_heros[3]);
+console.log(marvel_heros[3][2]);   
+
 
 marvel_heros.concat(dc_heros);
 console.log(marvel_heros);
-//  .push fuction only push the value in existion values
+//  .push function only push the value in existion values
 //  .concat function while pus it . it will return as a new array
 
 const All_Heros = marvel_heros.concat(dc_heros);
@@ -19,5 +22,16 @@ console.log( All_Heros_2 );   //spread the values
 
 const array_02 = [[1, 2, [2, 9, 4, 1, [13, 32], 32], 12, 7], [5, 6, 8]]
 const another_sub_array  = array_02.flat(Infinity);
-console.log(another_sub_array);     
+console.log(another_sub_array);    //
+console.log(Array.isArray(" Ayushman "));   //'.isArray' checks weather the given object is arrays or not.
+console.log(Array.from(" Ayushman "));     //'.from' converts thhe object into arrays
+console.log(Array.from({name : "Ayushman"}));    // here it prints empity . ause ere we declare its key-function.
+
+let score1= 200
+let score2= 500
+let score3= 400
+let score4= 200
+let score5= "Adhya"
+
+console.log(Array.of(score1, score2, score3, score4, score5));   //'.of' 
 
