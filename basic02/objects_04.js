7c95075d0b124a9dfe5cbf50f03f76b61b86e1ff
Ayushman Rout <161948217+ayushm001@users.noreply.github.com>
2024-05-16 23:55:31 +0530

    Update objects_04.js

--- basic02/objects_04.js
+++ basic02/objects_04.js
@@ -19,34 +19,14 @@ const RegularUser = {
     }
 }
 // console.log(RegularUser.email);
-// console.log(RegularUser.fullName.Last_name);
+//console.log(RegularUser.fullName.Last_name);
+//console.log(RegularUser.fullName?.Last_name);   // Here the symboll "?" provides extra protection.  
+ 
 
-const obj1 = {Id :"01", name: "Ayushman"}
-const obj2 = {Id :"02", name: "Sameeksha"}
-const obj3 = {Id :"03", name: "Bharghav"}
+ const obj1 = {1: "A", 2: "B"}
+ const obj2 = {3: "X", 4: "D"}
+ const obj3 = {5: "V", 6: "K"}
 
-// const obj4  = {obj1, obj2}
-// 
 
-// const obj4 = Object.assign({},obj1, obj2);
-// console.log(obj4);
-
-// const obj4 = {...obj1, ...obj2}
-// console.log(obj4);
-
-const User = [
-    {
-        Id: "1", 
-        email: "rout@gmail.com"
-    }
-    {
-        Id: "2", 
-        email: "rvet@gmail.com"
-    }
-    {
-        Id: "1", 
-        email: "rexo@gmail.com"
-    }
- ]
-// User[2].email
-// console.log(User); 
+const obj4 = {...obj1, ... obj2}
+console.log(obj4);
