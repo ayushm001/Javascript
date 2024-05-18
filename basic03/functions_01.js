ec985cfb472c95c28a014d6a6f1ae1a29bd5652b
Ayushman Rout <161948217+ayushm001@users.noreply.github.com>
2024-05-18 23:48:13 +0530

    Update functions_01.js

--- basic03/functions_01.js
+++ basic03/functions_01.js
@@ -20,6 +20,7 @@ function addTwoNumbers(number1, number2){
     // return result
     return number1 + number2
 }
+
 const result = addTwoNumbers(3, 5)
 
 // console.log("Result: ", result);
@@ -40,15 +41,3 @@ function loginUserMessage(username = "sam"){
 function calculateCartPrice(val1, val2, ...num1){
     return num1
 }
-
-// console.log(calculateCartPrice(200, 400, 500, 2000))
-
-const user = {
-    username: "hitesh",
-    prices: 199
-}
-
-function handleObject(anyobject){
-    console.log(`Username is ${anyobject.username} and price is ${anyobject.price}`);
-}
-
