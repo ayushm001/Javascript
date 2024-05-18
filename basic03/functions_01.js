75d205c828edffd8606afb605ecb66f06c22006e
Ayushman Rout <161948217+ayushm001@users.noreply.github.com>
2024-05-18 23:50:33 +0530

    Update functions_01.js

--- basic03/functions_01.js
+++ basic03/functions_01.js
@@ -1,8 +1,7 @@
 function sayMyName(){
-    console.log("H");
-    console.log("I");
-    console.log("T");
-    console.log("E");
+    console.log("A");
+    console.log("Y");
+    console.log("U");
     console.log("S");
     console.log("H");
 }
