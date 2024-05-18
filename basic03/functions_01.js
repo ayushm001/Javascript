43567c0d658356912f2fcba6b18206e1be82a957
Ayushman Rout <161948217+ayushm001@users.noreply.github.com>
2024-05-19 01:01:30 +0530

    Update functions_01.js

--- basic03/functions_01.js
+++ basic03/functions_01.js
@@ -1,67 +1,15 @@
-function sayMyName(){
+
+
+function Myname() {
     console.log("A");
     console.log("Y");
     console.log("U");
     console.log("S");
     console.log("H");
+    console.log("M");
+    console.log("A");
+    console.log("N");
 }
 
-// sayMyName()
+Myname()       //   The " () " refers to execute the function. But te simple Function can represent to function's reference 
 
-// function addTwoNumbers(number1, number2){
-
-//     console.log(number1 + number2);
-// }
-
-function addTwoNumbers(number1, number2){
-
-    // let result = number1 + number2
-    // return result
-    return number1 + number2
-}
-
-const result = addTwoNumbers(3, 5)
-
-// console.log("Result: ", result);
-
-
-function loginUserMessage(username = "sam"){
-    if(!username){
-        console.log("PLease enter a username");
-        return
-    }
-    return `${username} just logged in`
-}
-// console.log(calculateCartPrice(200, 400, 500, 2000))
-
-const user = {
-    username: "ayushman",
-    prices: 199
-}
-
-function handleObject(anyobject){
-    console.log(`Username is ${anyobject.username} and price is ${anyobject.price}`);
-}
-
-// handleObject(user)
-handleObject({
-    username: "sam",
-    price: 399
-})
-
-const myNewArray = [200, 400, 100, 600]
-
-function returnSecondValue(getArray){
-    return getArray[1]
-}
-
-// console.log(returnSecondValue(myNewArray));
-console.log(returnSecondValue([200, 400, 500, 1000]));
-
-// console.log(loginUserMessage("hitesh"))
-// console.log(loginUserMessage("hitesh"))
-
-
-function calculateCartPrice(val1, val2, ...num1){
-    return num1
-}
