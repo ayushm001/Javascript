c1ff0de1386ee439e9133e26bcf59b3a36896c31
Ayushman Rout <161948217+ayushm001@users.noreply.github.com>
2024-05-18 23:48:56 +0530

    Update functions_01.js

--- basic03/functions_01.js
+++ basic03/functions_01.js
@@ -33,6 +33,31 @@ function loginUserMessage(username = "sam"){
     }
     return `${username} just logged in`
 }
+// console.log(calculateCartPrice(200, 400, 500, 2000))
+
+const user = {
+    username: "hitesh",
+    prices: 199
+}
+
+function handleObject(anyobject){
+    console.log(`Username is ${anyobject.username} and price is ${anyobject.price}`);
+}
+
+// handleObject(user)
+handleObject({
+    username: "sam",
+    price: 399
+})
+
+const myNewArray = [200, 400, 100, 600]
+
+function returnSecondValue(getArray){
+    return getArray[1]
+}
+
+// console.log(returnSecondValue(myNewArray));
+console.log(returnSecondValue([200, 400, 500, 1000]));
 
 // console.log(loginUserMessage("hitesh"))
 // console.log(loginUserMessage("hitesh"))
