6585de1d02f6c9aa99794200fec0972674ecacbc
Ayushman Rout <161948217+ayushm001@users.noreply.github.com>
2024-05-25 23:59:20 +0530

    Update scopes_02.js

--- basic03/scopes_02.js
+++ basic03/scopes_02.js
@@ -50,3 +50,19 @@ if (true) {
 }
 
 // console.log(username);
+
+// ++++++++++++++++++ interesting ++++++++++++++++++
+
+
+console.log(addone(5))
+
+function addone(num){
+    return num + 1
+}
+
+
+
+addTwo(5)
+const addTwo = function(num){
+    return num + 2
+}
