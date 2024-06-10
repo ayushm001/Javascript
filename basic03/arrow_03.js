5d133d2040556d70dd5a309a570c2c4c119be97e
Ayushman Rout <161948217+ayushm001@users.noreply.github.com>
2024-06-11 00:13:52 +0530

    Update arrow_03.js

--- basic03/arrow_03.js
+++ basic03/arrow_03.js
@@ -1,56 +1,41 @@
-
-
 const user = {
-    username: "ayushman ",
-    price: 999,
+    username : "Ayushamn",
+    price : 999,
 
-    welcomeMessage: function() {
-        console.log(`${this.username} , welcome to website`);
-        console.log(this);
+    welcomeMessage: function(){
+
+        console.log(`${this.username}, Welcome to website`);
+        console.log(this)
     }
-
+ 
 }
-
 // user.welcomeMessage()
-// user.username = "sam"
+// user.username = "Ankit"
 // user.welcomeMessage()
-
 // console.log(this);
+// console.log(welcomeMessage()); 
 
-// function chai(){
-//     let username = "ayushman"
+// function desktop() {
+//     let username = "Ayushman"
 //     console.log(this.username);
 // }
+// desktop()
 
-// chai()
-
-// const chai = function () {
-//     let username = "ayushman "
+// const desktop = () => {
+//     let username= "ayushman"
 //     console.log(this.username);
 // }
+// desktop()
 
-const chai =  () => {
-    let username = "ayushman "
-    console.log(this);
-}
+// Basic syntax of arrow finction :-    () => {}
 
-
-// chai()
-
-// const addTwo = (num1, num2) => {
-//     return num1 + num2
+// const addtwo = (num1, num2) => {
+//     return num1+num2
+  
 // }
-
-// const addTwo = (num1, num2) =>  num1 + num2
-
-// const addTwo = (num1, num2) => ( num1 + num2 )
-
-const addTwo = (num1, num2) => ({username: "hitesh"})
+// console.log(addtwo(2, 3));             // basic arrow function
 
 
-console.log(addTwo(3, 4))
-
-
-// const myArray = [2, 5, 3, 7, 8]
-
-// myArray.forEach()
+// const addtwo = (num1 , num2) => num1 + num2
+const addtwo = (num1, num2) => (num1+num2)
+console.log(addtwo(3, 4));
