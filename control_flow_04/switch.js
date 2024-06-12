da5c0cf94c27ce1f67e00a816844a7160aa764c3
Ayushman Rout <161948217+ayushm001@users.noreply.github.com>
2024-06-13 01:36:28 +0530

    Update switch.js
    switch case use

---
+++ control_flow_04/switch.js
@@ -0,0 +1,34 @@
+//  switch case 
+
+const week= 5
+
+switch (week) {
+    case 1:
+        console.log("sunday");
+        break;
+    case 2:
+        console.log("monday");
+        break;
+    case 3:
+        console.log("tuesday");
+        break;
+    case 4:
+        console.log("wednesday");
+        break;
+    case 5:
+        console.log("thursday");
+        break;
+    case 6:
+        console.log("friday");
+        break;
+    case 7:
+        console.log("saturday");
+        break;
+
+    default:
+        console.log("please enter a valid number");
+        break;
+}
+
+
+
