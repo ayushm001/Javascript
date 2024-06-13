8cd5c8c90d31652caa813696241c2ac8a2f04b0e
Ayushman Rout <161948217+ayushm001@users.noreply.github.com>
2024-06-14 03:01:25 +0530

    do- while  loop
    ->  while loop
    ->  loop   x   Array
    ->  Do while loop

---
+++ iterations_05/do_while_02.js
@@ -0,0 +1,28 @@
+//  while loop :-
+
+let index = 0
+while (index <= 10) {
+    console.log(`values of index is ${index}`);
+    index = index + 2
+}
+
+
+//  array  x  loop
+
+let MyArr = ["Bhubaneshwar", "cuttuck", "puri", "khordha"]
+let arr =2
+while (arr < MyArr.length) {
+    console.log(`value is ${MyArr[arr]}`);
+    arr =arr +1
+    
+}
+
+
+// do while  loop :--
+
+let score = 200
+
+do {
+    console.log(`score is ${score }`);
+    score ++
+} while (score<= 100);
