31fcff279ca2daaec017289a768891c0b84d01aa
Ayushman Rout <161948217+ayushm001@users.noreply.github.com>
2024-07-17 02:25:29 +0530

    fully working
    addng function , conditons (loops)

--- DOM(Projects)_07/1-Color_Changer/script.js
+++ DOM(Projects)_07/1-Color_Changer/script.js
@@ -1,8 +1,32 @@
-const buttion = document.querySelectorAll('buttion')
+
+const buttion = document.querySelectorAll('.button')
 const body = document.querySelector('body')
 
 // console.log(body);
 
-buttion.forEach(function (buttion) {
-    buttion.addEventListner()
-})
+
+buttion.forEach(function (button) {
+    console.log(button);
+    button.addEventListener('click' , function(event){
+        console.log(event);
+        console.log(event.target);
+        if (event.target.id === 'red') {
+            body.style.backgroundColor = event.target.id
+        } 
+           // we can also target its hardcore value . otherwise defined it with id
+        if (event.target.id ==='green') {
+            body.style.backgroundColor = event.target.id
+        }
+        if (event.target.id === 'blue') {
+            body.style.backgroundColor = 'blue'
+        }
+        if (event.target.id === 'grey') {
+            body.style.backgroundColor = 'grey'
+        }
+        if (event.target.id === 'yellow') {
+            body.style.backgroundColor = event.target.id
+        }
+    });
+});
+
+// We can also use it through Switch-case
