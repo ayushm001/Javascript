7baba1861f253ae1eec625e3106f5f1a9909ccd8
Ayushman Rout <161948217+ayushm001@users.noreply.github.com>
2024-07-20 01:40:33 +0530

    Update script.js
    Adding  BMI-Suggestion part

--- DOM(Projects)_07/2-BMI_Calculator/script.js
+++ DOM(Projects)_07/2-BMI_Calculator/script.js
@@ -2,10 +2,8 @@ const form = document.querySelector('form')
 
 form.addEventListener('submit', function (x){
     x.preventDefault()
-    // .preventDefault() -> cancels the event (default action that belongs to event will not occure )
-    //                       means it did not open a new pages and hold it.
+    // .preventDefault() 
 
-  
     const weight = parseInt(document.querySelector('#KG').value)
     const height = parseInt(document.querySelector('#CM').value)
 
@@ -13,6 +11,8 @@ form.addEventListener('submit', function (x){
     //  parseInt -> to obtain only int values 
 
     const result = document.querySelector('.result')
+    let bmi      // this will help to display the BMI- suggestion part
+
     // applying condition 
     if (weight === '' || weight < 0 || isNaN(weight) ) {
         result.innerHTML= `Please give a Valid value ${weight}`
@@ -25,5 +25,21 @@ form.addEventListener('submit', function (x){
         // .toFixed(2) ->  the result will display upto 2 decimal places.
 
         result.innerHTML = `<span>${bmi} is your Body-Mass-Index</span>`
+    
+
+    // Display guide sugestion 
+
+    const adv= document.querySelector('.sugg')
+    // applying BMI-suggesstion
+
+    if (bmi <= 18.6) {
+        adv.innerHTML = `<span> you are under weight ${bmi} </span>`
+    } else if (bmi >= 24.9) {
+        adv.innerHTML = `<span> you are over weight. you need a good diet  ${bmi}</span>`
+    } else {
+        adv.innerHTML = `<span> Congartulation you are in normal range, ${bmi} </span>`
     }
+    }  //  we need to hold all the suggestion part  in result_else()- block, 
+       //  this will help to  display the bmi-result in suuggestion part (SCOPE- concept)
+
 })
