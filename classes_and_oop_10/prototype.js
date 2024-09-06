000c0a24d6adc14832720173d028706df7702737
Ayushman Rout <161948217+ayushm001@users.noreply.github.com>
2024-09-06 20:12:34 +0530

    Update prototype.js
    ->  prototype concept
    ->  prototypical inheritance 
    ->  trim( )

--- classes_and_oop_10/prototype.js
+++ classes_and_oop_10/prototype.js
@@ -37,4 +37,62 @@ club.Name()
 
 club.New_name()  // here the club is an object not an array !!
 //  The New_name method is added to Array.prototype, which means it is available to all array instances, but not to objects.
-Players.New_name()
+Players.New_name() 
+
+
+//  INHERITANCE
+
+ /* 
+ 
+  Prototypal inheritance in JavaScript is a way for objects to inherit properties and methods from other objects.
+  This is different from classical inheritance, where classes inherit from other classes.
+
+
+   Property: Every JavaScript object has a hidden property called [[Prototype]] (or __proto__ in older syntax), which points to another object.
+             This other object is called the prototype.
+
+    Prototype chain : When you try to access a property or method on an object, JavaScript first looks at the object itself. If it doesn’t find it there, it looks at the object’s prototype,
+                      and then the prototype’s prototype, and so on, until it finds the property/method or reaches the end of the chain (null).
+ 
+ */
+
+
+
+const automobile = {
+      name: "Bike",
+      safety : false,
+    }
+    
+    const car = {
+        safety: true
+    }
+    
+    const power = {
+        isAvalible : true
+    }
+    
+    const BMW = {
+        Speed : true,
+        comfort : true,
+        __proto__: car
+    }
+    
+    automobile.__proto__ = car   // outdated approach
+    
+    
+    Object.setPrototypeOf(power, automobile) // modern syntax
+
+
+    
+ let Ply_name = "Thomas Muller              "
+ 
+ String.prototype.OrignalLength = function(){
+     console.log(`${this}`);  
+     console.log(this.name);  // undefined cause name is not a property of string 
+     console.log(`Origanl lengt is  -> ${this.trim().length}`);
+ }
+ 
+ Ply_name.OrignalLength()
+ 
+ "AYUSHHMAN".OrignalLength()
+ "cherry            ".OrignalLength()
