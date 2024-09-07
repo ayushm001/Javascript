dfa6ed3ac6c41aa87806e6be9c688d984a35f6de
Ayushman Rout <161948217+ayushm001@users.noreply.github.com>
2024-09-07 23:49:24 +0530

    Add files via upload

---
+++ classes_and_oop_10/myClasses.js
@@ -0,0 +1,43 @@
+
+
+class User {
+    constructor(username, email, password){
+        this.username = username;
+        this.email = email;
+        this.password = password
+    }
+
+    encryptPassword(){
+        return `${this.password}abc`
+    }
+    changeUsername(){
+        return `${this.username.toUpperCase()}`
+    }
+
+}
+
+const chai = new User("person", "person@gmail.com", "123")
+
+console.log(chai.encryptPassword());
+console.log(chai.changeUsername());
+
+// behind the scene
+
+function User(username, email, password){
+    this.username = username;
+    this.email = email;
+    this.password = password
+}
+
+User.prototype.encryptPassword = function(){
+    return `${this.password}abc`
+}
+User.prototype.changeUsername = function(){
+    return `${this.username.toUpperCase()}`
+}
+
+
+const Women = new User("Natasha", "Natasha@gmail.com", "521")
+
+console.log(Women.changeUsername());
+console.log(Women.encryptPassword());
